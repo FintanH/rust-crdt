745a7d18aae249a6261674a5ade95bce852bd19a
FintanH <fintan.halpenny@gmail.com>
2020-04-10 22:43:01 +0100

    Update tests to new code

--- test/lseq.rs
+++ test/lseq.rs
@@ -1,17 +1,17 @@
-
-use crdts::lseq::*;
 use crdts::lseq::ident::*;
+use crdts::lseq::*;
+use crdts::CmRDT;
 use rand::distributions::Alphanumeric;
 use rand::Rng;
 
 #[test]
 fn test_out_of_order_inserts() {
-    let mut site1 = LSeq::new(0);
-    site1.local_insert(0, 'a');
-    site1.local_insert(1, 'c');
-    site1.local_insert(1, 'b');
+    let mut site1 = LSeq::new(SiteId::new(0));
+    site1.insert_index(0, 'a');
+    site1.insert_index(1, 'c');
+    site1.insert_index(1, 'b');
 
-    assert_eq!(site1.sequence().collect::<String>(), "abc");
+    assert_eq!(site1.iter().collect::<String>(), "abc");
 }
 
 #[test]
@@ -24,19 +24,28 @@ fn test_inserts() {
     let mut s1 = rng.sample_iter(Alphanumeric);
     let mut s2 = rng.sample_iter(Alphanumeric);
 
-    let mut site1 = LSeq::new(0);
-    let mut site2 = LSeq::new(1);
+    let mut site1 = LSeq::new(SiteId::new(0));
+    let mut site2 = LSeq::new(SiteId::new(1));
 
     for _ in 0..5000 {
         if rng.gen() {
-            let op = site1.local_insert(rng.gen_range(0, site1.raw_entries().len() + 1), s1.next().unwrap());
-            site2.apply(&op);
+            let op = site1.insert_index(
+                rng.gen_range(0, site1.raw_entries().len() + 1),
+                s1.next().unwrap(),
+            );
+            site2.apply(op);
         } else {
-            let op = site2.local_insert(rng.gen_range(0, site2.raw_entries().len() + 1), s2.next().unwrap());
-            site1.apply(&op);
+            let op = site2.insert_index(
+                rng.gen_range(0, site2.raw_entries().len() + 1),
+                s2.next().unwrap(),
+            );
+            site1.apply(op);
         }
     }
-    assert_eq!(site1.sequence().collect::<String>(), site2.sequence().collect::<String>());
+    assert_eq!(
+        site1.iter().collect::<String>(),
+        site2.iter().collect::<String>()
+    );
 }
 
 #[derive(Clone)]
@@ -55,13 +64,13 @@ impl Arbitrary for OperationList {
             }
         };
 
-        let mut site1 = LSeq::new(g.gen());
+        let mut site1 = LSeq::new(SiteId::new(g.gen()));
         let ops = (0..size)
-            .map(|_| {
+            .filter_map(|_| {
                 if g.gen() || site1.len() == 0 {
-                    site1.local_insert(g.gen_range(0, site1.len() + 1), g.gen())
+                    site1.delete_index(g.gen_range(0, site1.len() + 1))
                 } else {
-                    site1.local_delete(g.gen_range(0, site1.len()))
+                    site1.delete_index(g.gen_range(0, site1.len()))
                 }
             })
             .collect();
@@ -76,8 +85,8 @@ fn prop_inserts_and_deletes() {
     let mut op1 = OperationList::arbitrary(&mut rng).0.into_iter();
     let mut op2 = OperationList::arbitrary(&mut rng).0.into_iter();
 
-    let mut site1 = LSeq::new(0);
-    let mut site2 = LSeq::new(1);
+    let mut site1 = LSeq::new(SiteId::new(0));
+    let mut site2 = LSeq::new(SiteId::new(1));
 
     let mut s1_empty = false;
     let mut s2_empty = false;
@@ -85,8 +94,8 @@ fn prop_inserts_and_deletes() {
         if rng.gen() {
             match op1.next() {
                 Some(o) => {
-                    site1.apply(&o);
-                    site2.apply(&o);
+                    site1.apply(o.clone());
+                    site2.apply(o);
                 }
                 None => {
                     s1_empty = true;
@@ -95,8 +104,8 @@ fn prop_inserts_and_deletes() {
         } else {
             match op2.next() {
                 Some(o) => {
-                    site1.apply(&o);
-                    site2.apply(&o);
+                    site1.apply(o.clone());
+                    site2.apply(o);
                 }
                 None => {
                     s2_empty = true;
@@ -105,9 +114,8 @@ fn prop_inserts_and_deletes() {
         }
     }
 
-    let site1_text = site1.sequence().collect::<String>();
-    let site2_text = site2.sequence().collect::<String>();
+    let site1_text = site1.iter().collect::<String>();
+    let site2_text = site2.iter().collect::<String>();
 
     assert_eq!(site1_text, site2_text);
 }
-
